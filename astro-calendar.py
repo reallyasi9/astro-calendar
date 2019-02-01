d831f6c82f9928f81c0a7387f2b9f8cdc8197648
Phil Killewald <pkillewald@mathematica-mpr.com>
2019-02-01 18:13:02 -0500

    feat: calculate day angles, radii, and sunrise/set times

--- astro-calendar.py
+++ astro-calendar.py
@@ -6,7 +6,9 @@ from skyfield import almanac
 import yaml
 import sys
 import pytz
-from datetime import datetime
+from datetime import datetime, timedelta
+import math
+import numpy as np
 
 
 @click.command()
@@ -19,28 +21,30 @@ def astro_calendar(config_file, output_file):
     output = open_output_file(output_file)
 
     ts = sf.load.timescale()
-    eph = sf.load("de435.bsp")
+    eph = sf.load(config["ephemeris"])
+    tz = pytz.timezone(config["timezone"])
+    year = config["year"]
 
-    timezone = pytz.timezone(config["timezone"])
-    t0 = ts.utc(datetime(2018, 12, 21, tzinfo=timezone))
-    t1 = ts.utc(datetime(2019, 6, 21, tzinfo=timezone))
-
-    times, idx = almanac.find_discrete(t0, t1, almanac.seasons(eph))
-    for i, time in zip(idx, times):
-        print(i, almanac.SEASON_EVENTS[i], time.utc_iso(" "))
-
-    sun = eph["sun"]
-    earth = eph["earth"]
-    boston = earth + sf.Topos(
+    offset = sf.Topos(
         latitude_degrees=config["location"]["lat"],
         longitude_degrees=config["location"]["lon"],
         elevation_m=config["location"]["alt"],
     )
-    # heliocentric = sun.at(t0).observe(boston)
-    # Note:  in this frame, the winter solstace is roughly +y, the spring equinox is roughly -x
-    print(sun.at(t0).observe(boston).position.au)
-    print(sun.at(t1).observe(boston).position.au)
+    sun = eph["sun"]
+    observed = eph["earth"] + offset
 
+    # # Note:  in this frame, the winter solstace is roughly +y, the spring equinox is roughly -x
+    day_radials = calculate_day_radials(ts, sun, observed, tz, year)
+    sunrises, sunsets = calculate_sunrises_sunsets(eph, ts, offset, tz, year)
+
+    print(sunrises.utc_iso())
+    print(sunsets.utc_iso())
+
+
+    # times, idx = almanac.find_discrete(t0, t1, almanac.seasons(eph))
+    # for i, time in zip(idx, times):
+    #     print(i, almanac.SEASON_EVENTS[i], time.utc_iso(" "))
+    
     output.close()
 
 
@@ -61,6 +65,33 @@ def open_output_file(output_file):
         return sys.stdout
     else:
         return open(output_file, "w")
+
+
+def _t_generator(ts, t, delta_t):
+    start_year = t.year
+    while t.year == start_year:
+        yield ts.utc(t)
+        t += delta_t
+
+
+def calculate_day_radials(ts, observer, observed, tz, year):
+    t = datetime(year, 1, 1, tzinfo=tz)
+    delta_t = timedelta(days=1)
+    return np.vstack([day_radial(observer, observed, t_utc) for t_utc in _t_generator(ts, t, delta_t)])
+
+
+def day_radial(observer, observed, t_utc):
+    pos = observer.at(t_utc).observe(observed).position.au
+    return np.array([np.linalg.norm(pos), math.atan2(pos[1], pos[0])])
+
+
+def calculate_sunrises_sunsets(eph, ts, offset, tz, year):
+    start_day = ts.utc(datetime(year, 1, 1, tzinfo=tz))
+    end_day_excl = ts.utc(datetime(year+1, 1, 1, tzinfo=tz))
+    sunrise_sunset, is_sunrise = almanac.find_discrete(start_day, end_day_excl, almanac.sunrise_sunset(eph, offset))
+    sunrises = sunrise_sunset[is_sunrise]
+    sunsets = sunrise_sunset[~is_sunrise]
+    return sunrises, sunsets
 
 
 if __name__ == "__main__":
